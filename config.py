a69de5b14f0cd7bec8c90a2ef755f78d7e7d034e
sherifadelsaleh15 <sherifevolvice@gmail.com>
2026-02-10 11:19:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 APP_TITLE = "2026 Strategy Intelligence Hub"
-SHEET_ID = "1QFIhc5g1FeMj-wQSL7kucsAyhgurxH9mqP3cmC1mcFY"
+SHEET_ID = "https://docs.google.com/spreadsheets/d/1QFIhc5g1FeMj-wQSL7kucsAyhgurxH9mqP3cmC1mcFY/edit?usp=sharing"
 
 # These must match your Google Sheet tabs exactly
 TABS = [
