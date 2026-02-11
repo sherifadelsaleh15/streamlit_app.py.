2278eee51a9e7ce445bd2a88601f7aa2a172151f
sherifadelsaleh15 <sherifevolvice@gmail.com>
2026-02-11 13:41:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-"GA4_Top_Pages", 
-"GSC", 
-"SOCIAL_MEDIA", 
-    "MASTER_FEED"
+USE_SAMPLE_DATA = False # Set to True to test without Google Sheets
+SHEET_ID = "1QFIhc5g1FeMj-wQSL7kucsAyhgurxH9mqP3cmC1mcFY"
+
+TABS = [
+    "GA4_Top_Pages", 
+    "GSC", 
+    "SOCIAL_MEDIA", 
     "MASTER_FEED",
-    "Position_Tracking_Standard"  # <--- Add this line
+    "Position_Tracking_Standard"
 ]
