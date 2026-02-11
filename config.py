37ed25f25a1097cb40f223a01034d90183dac613
sherifadelsaleh15 <sherifevolvice@gmail.com>
2026-02-11 13:54:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ SHEET_ID = "1QFIhc5g1FeMj-wQSL7kucsAyhgurxH9mqP3cmC1mcFY"
 USE_SAMPLE_DATA = False
 
 TABS = [
+    "GA4_Data",  # Ensure this matches your Google Sheet tab name exactly
     "GA4_Top_Pages", 
     "GSC", 
     "SOCIAL_MEDIA", 
